dcea13b94d7f97cbd77bc18da165b8e51934e0d7
Nicholas Pappagalo <npappagalo@gmail.com>
2015-07-10 00:47:40 -0700

    Basic JS Greeting Field

---
+++ ch02/js/example.js
@@ -0,0 +1,31 @@
+var greeting = 'Howdy ';
+var name = 'Molly';
+var message = ', please check your order: ';
+var welcome = greeting + name + message;
+
+// Details about the sign
+var sign = 'Montague House';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+// Get element that has an id of greeting
+var el = document.getElementById('greeting');
+// Replace the content of that element with the personalized welcome message
+el.textContent = welcome;
+
+var elSign = document.getElementById('userSign');
+elSign.textContent = sign;
+
+var elTiles = document.getElementById('tiles');
+elTiles.textContent = tiles;
+
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent = '$' + subTotal;
+
+var elShipping = document.getElementById('shipping');
+elShipping.textContent = '$' + shipping;
+
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent = '$' + grandTotal;
